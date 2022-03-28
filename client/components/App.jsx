359f66f4429ac14be2ed2ef1ed360a508397f7e7
Murphy Fleenor <jmfleeno@gmail.com>
2022-03-28 14:52:45 -0500

    Created Front End Component Skeloton Structure

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,14 @@
+// Import Dependencies
 import React from "react";
-import ReactDOM from "react-dom";
 import { useEffect, useState } from "react";
+
+// Import Components
+// import Login from "./Login.jsx"
+import Quartermaster from "./Quartermaster.jsx"
+import TrailFeathers from "./TrailFeathers.jsx"
+import Trails from "./Trails.jsx"
+import TrailsList from "./TrailsList.jsx"
+import UserProfile from "./UserProfile.jsx"
 
 const App = () => {
   return (
@@ -9,6 +17,12 @@ const App = () => {
         Take a Hike in Louisiana
       </h1>
       <h2>All parks within 500 miles radius</h2>
+      {/* <Login/> */}
+      <UserProfile/>
+      <TrailsList/>
+      <Trails/>
+      <Quartermaster/>
+      <TrailFeathers/>
     </>
   );
 };
--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,12 @@
-"use strict";
+"use strict"; // Why is this up here?
 
+// Import Dependencies
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.jsx";
 
+// Import Components and Styles
+import App from './components/App.jsx';
+import './styles/main.css';
+
+// Render Components to DOM 'app' element
 ReactDOM.render(<App />, document.getElementById("app"));
