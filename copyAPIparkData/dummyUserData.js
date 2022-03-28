61184bbeea8a1affbad4ec7b16f1e55d48963f98
Rene Mercadel <renemercadel@gmail.com>
2022-03-28 16:28:07 -0500

    Create files for Users, User Photos, Birds, and Paking List Items data. Create dummy data for Users and Birds

---
+++ copyAPIparkData/dummyUserData.js
@@ -0,0 +1,32 @@
+const dummyUserData = [
+  {
+    name: 'Cornelle Dennerley',
+  },
+  {
+    name: 'Lorettalorna Phillips',
+  },
+  {
+    name: 'Francklin Eakle',
+  },
+  {
+    name: 'Merrily McGillecole',
+  },
+  {
+    name: 'Carla Kira',
+  },
+  {
+    name: "Reggi D'Ruel",
+  },
+  {
+    name: 'Thibaud Harbar',
+  },
+  {
+    name: 'Greer Rudge',
+  },
+  {
+    name: 'Bennett Starte',
+  },
+  {
+    name: 'Humberto Lakin',
+  },
+];
