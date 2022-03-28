e85b3687f48a03fa61a9b27dd4b524180db51176
Santolococo <santolococo@gmail.com>
2022-03-28 14:22:16 -0600

    made middleware folder with routes folder and auth file. added login.jsx to components. started to add code for auth

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const passport = require('passport');
 
 //handles parsing content in the req.body from post/update requests
 app.use(express.json());
