d43f5624466a774fb809b74285611b0f06b9e041
Santolococo <santolococo@gmail.com>
2022-03-29 15:14:50 -0600

    adding users aspect to function in auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const { Users } = require('../database/models/users.js')
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
@@ -13,7 +14,7 @@ passport.use(new GoogleStrategy({
   passReqToCallback: true
 },
   function (request, accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    Users.findOrCreate({ googleId: profile.id }, function (err, user) {
       return done(err, user);
     });
   }
