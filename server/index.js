5b60dcb5f579bd4f1cad2d06c366b19acc464be5
Santolococo <santolococo@gmail.com>
2022-03-29 15:00:55 -0600

    moved middleware folder. made basic routing for login and added auth.js to middleware with basic passport stuff

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const router = express.Router();
 
 //handles parsing content in the req.body from post/update requests
 app.use(express.json());
@@ -19,6 +20,21 @@ app.use(express.static(distPath));
 //   // Sending This is the home page! in the page
 //   res.status(200).send(app.use(express.static(distPath)));
 // });
+
+// Routes
+
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with google</a>')
+});
+
+app.get('/!!user')
+
+
+
+router.get('/login', function(req, res, next) {
+  res.render('login')
+});
+
 
 // launches the server from localhost on port 5555
 let PORT = 5555;
---
+++ server/middleware/auth.js
@@ -0,0 +1,20 @@
+//todo 
+// first i need to create a new project in google developers console
+// then i will be given a client ID and client secret which must be provided to passport
+// then i will need to configure a redirect URI with matches the rout in our application.
+require('dotenv').config();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:5555/google/callback",
+  passReqToCallback: true
+},
+  function (request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
