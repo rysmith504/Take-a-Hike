288bba857ef6242feee35f73e446a10c88af6c88
Rmachirica <rodolfomachirica@gmail.com>
2022-03-30 16:40:06 -0500

    pulling changes from upstream

---
+++ client/components/PackingList.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+
+const PackingList = (props) => {
+  //a listnames coming from quartermaster
+  //allow users to populate list with items
+};
+
+export default PackingList;
