3ae723b92ab60f3745018c057ce0322e758617d6
Rmachirica <rodolfomachirica@gmail.com>
2022-03-30 00:13:38 -0500

    Create packingList component, set up get and post requests

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 
 // Import Components
 // import Login from "./Login.jsx"
-import Quartermaster from "./Quartermaster.jsx"
-import TrailFeathers from "./TrailFeathers.jsx"
-import Trails from "./Trails.jsx"
-import TrailsList from "./TrailsList.jsx"
-import UserProfile from "./UserProfile.jsx"
+import Quartermaster from "./Quartermaster.jsx";
+import TrailFeathers from "./TrailFeathers.jsx";
+import Trails from "./Trails.jsx";
+import TrailsList from "./TrailsList.jsx";
+import UserProfile from "./UserProfile.jsx";
 
 const App = () => {
   return (
@@ -18,11 +18,10 @@ const App = () => {
       </h1>
       <h2>All parks within 500 miles radius</h2>
       {/* <Login/> */}
-      <UserProfile/>
-      <TrailsList/>
-      <Trails/>
-      <Quartermaster/>
-      <TrailFeathers/>
+      {/* <UserProfile /> */}
+      <Quartermaster />
+      {/* <Trails />
+      <TrailFeathers /> */}
     </>
   );
 };
