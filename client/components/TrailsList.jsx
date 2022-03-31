171c043fc12612a777f7b851e66bcd292c28c5a1
John Dyer <jldyer85@gmail.com>
2022-03-30 21:50:43 -0500

    set up route handler for lat/lon client req - not complete

--- client/components/TrailsList.jsx
+++ client/components/TrailsList.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from "react";
-import TrailsListEntry from './TrailsListEntry.jsx'
-
+import TrailsListEntry from './TrailsListEntry.jsx';
+import axios from 'axios';
 
 const TrailsList = ({dummyData}) => {
 
-  const [location, setLocation] = useState({ lat: "", lon: ""})
+  const [location, setLocation] = useState({ lat: "", lon: ""});
+  const [trailList, setTrailList] = useState([]);
 
-  const handleChange = (e) => {
+  const handleLocationInput = (e) => {
     const { name, value } = e.target
     setLocation((location) => {
       return {...location, [name]: value, [name]: value}
     })
-    console.log(location);
+  }
+
+  const handleSubmitLocation = (e) => {
+    e.preventDefault();
+    axios.get('/api/TrailsList', {params: { lat: location.lat, lon: location.lon }} )
+    .then((arrayOfObj) => {
+      console.log('Line 21 = ', arrayOfObj);
+      setTrailList((trailList) => {
+        return [...trailList, ...arrayOfObj]
+      })
+    })
+    .catch((err) => {
+      console.error('ERROR:', err);
+    })
   }
 
   return (
@@ -25,7 +39,7 @@ const TrailsList = ({dummyData}) => {
               placeholder="lat"
               className="card"
               value={location.lat}
-              onChange={handleChange}
+              onChange={handleLocationInput}
               name="lat"
             />
           </label>
@@ -35,14 +49,14 @@ const TrailsList = ({dummyData}) => {
               placeholder="lon"
               className="card"
               value={location.lon}
-              onChange={handleChange}
+              onChange={handleLocationInput}
               name="lon"
             />
           </label>
           <input
             type="submit"
             value="Send Location"
-            // onClick={this.handleLocation}
+            onClick={handleSubmitLocation}
           />
         </form>
       <div className='trails'>
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
+const axios = require("axios");
 
 // Set Distribution Path
 const PORT = 5555;
@@ -26,8 +27,18 @@ app.use(express.urlencoded({ extended: true })); // Parses url (allows arrays an
 // });
 
 //GET req all LA trail data from external API
-app.get('/', (req, res) => {
-  res.send();
+app.get('/api/TrailsList', (req, res) => {
+  // res.send();
+  // console.log('Line 31 = ', req.query);
+  axios.get(`https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=${req.query.lat}&lon=${req.query.lon}&radius=100`,)
+  .then(data => {
+    console.log('Line 34 = ', data);
+    res.send(data).status(200);
+  })
+  .catch((err) => {
+    console.error('ERROR: ', err);
+    res.sendStatus(404);
+  })
 })
 
 
