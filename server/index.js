1e68ad704938070766e9643fdef1653422fe365a
Rene Mercadel <renemercadel@gmail.com>
2022-04-01 14:41:54 -0500

    Merging with Santos Branch

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 // Import Dependencies
-const axios = require("axios");
-const { query } = require("express");
-const express = require("express");
-const path = require("path");
-const { PackingLists } = require("./database/models/packingLists");
+const axios = require('axios');
+const { query } = require('express');
+const express = require('express');
+const path = require('path');
+const { PackingLists } = require('./database/models/packingLists');
 // PackingLists.sync();
 // const { default: PackingList } = require("../client/components/PackingList");
 const router = express.Router();
-const session = require('express-session')
+const session = require('express-session');
 const passport = require('passport');
-require('./middleware/auth.js')
+require('./middleware/auth.js');
 const { cloudinary } = require('./utils/coudinary');
 
 // // Import DB
@@ -20,7 +20,7 @@ const { cloudinary } = require('./utils/coudinary');
 
 // Set Distribution Path
 const PORT = 5555;
-const distPath = path.resolve(__dirname, "..", "dist"); //serves the hmtl file of the application as default on load
+const distPath = path.resolve(__dirname, '..', 'dist'); //serves the hmtl file of the application as default on load
 
 // Create backend API
 const app = express();
@@ -29,51 +29,59 @@ const app = express();
 app.use(express.json()); // handles parsing content in the req.body from post/update requests
 app.use(express.static(distPath)); // Statically serves up client directory
 app.use(express.urlencoded({ extended: true })); // Parses url (allows arrays and objects)
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}))
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
 app.use(passport.initialize());
 // Create API Routes
 app.use(passport.session());
 
 //Auth Routes
 const checkAuthenticated = (req, res, next) => {
-  console.log(session)
-  if (req.isAuthenticated()) { return next() }
-  res.redirect("/login")
-}
+  console.log(session);
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
 
 app.get('/', (req, res) => {
-  res.send('<a href="/auth/google">Authenticate with google</a>')
+  res.send('<a href="/auth/google">Authenticate with google</a>');
 });
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: [ 'email', 'profile' ]
-}));
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] })
+);
 
-app.get('/auth/google/callback', passport.authenticate( 'google', {
-   successRedirect: '/dashboard',
-   failureRedirect: '/login'
-}));
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
+  })
+);
 
 app.get('/auth/failure', (req, res) => {
   res.send('did not authenticate');
-})
+});
 
 app.get('/dashboard', checkAuthenticated, (req, res) => {
   res.render('index', (err, html) => {
-    res.send(html)
+    res.send(html);
   });
-})
+});
 
-app.post("/logout", (req,res) => {
-  req.logOut()
-  res.redirect("/login")
-  console.log(`-------> User Logged out`)
-})
+app.post('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/login');
+  console.log(`-------> User Logged out`);
+});
 
 //Auth Routes end
 
@@ -86,15 +94,16 @@ app.post("/logout", (req,res) => {
 ////////////////////////////////////////EXTERNAL TRAIL API ROUTE/////////////////////////////////////////
 
 //GET req for trail data by latitude/longitude
-app.get("/api/trailslist", (req, res) => {
+app.get('/api/trailslist', (req, res) => {
   axios
     .get(
       `https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=${req.query.lat}&lon=${req.query.lon}&radius=100`,
       {
         headers: {
           'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com',
-          'X-RapidAPI-Key': 'a27adeb778msh22d13ed248d5359p1d95b8jsnb7239b396c5c'
-        }
+          'X-RapidAPI-Key':
+            'a27adeb778msh22d13ed248d5359p1d95b8jsnb7239b396c5c',
+        },
       }
     )
     .then((response) => {
@@ -102,7 +111,7 @@ app.get("/api/trailslist", (req, res) => {
       res.json(response.data);
     })
     .catch((err) => {
-      console.error("ERROR: ", err);
+      console.error('ERROR: ', err);
       res.sendStatus(404);
     });
 });
@@ -110,38 +119,42 @@ app.get("/api/trailslist", (req, res) => {
 //////////////////////////////////////// Cloudinary routes //////////////////////////////////////
 
 // get request to get all images (this will later be trail specific)
-app.get("/api/images", async (req, res) => {
+app.post('/api/images', async (req, res) => {
+  console.log(`server index.js || LINE 70`, req.body);
   // NEED TO CHANGE ENDPOINT TO INCLUDE TRAIL SPECIFIC PARAM SO PHOTOS CAN BE UPLOADED + RENDERED PROPERLY
 
   // Can create new folder with upload from TrailProfile component. Need to modify get request to filter based on folder param (which will be equal to the trail name)
-  const { resources } = await cloudinary.search
-    .expression('resource_type:image AND folder:"Trail 2"/*')
-    .sort_by("created_at", "asc")
+  const resources = await cloudinary.search
+    .expression(`resource_type:image AND folder:${req.body.trailFolderName}/*`)
+    .sort_by('created_at', 'asc')
     .max_results(30)
     .execute();
   // console.log(
   //   'SERVER INDEX.JS || CLOUDINARY GET || LINE 38 || resources ==>',
   //   resources
-  // ;
-  const secureImageUrls = resources.map((image) => image.secure_url);
+  // );
+  // try to filter before map
+  const secureImageUrls = resources.resources
+    .filter((imageObj) => imageObj.folder === req.body.trailFolderName)
+    .map((image) => image.secure_url);
   res.json(secureImageUrls);
 });
 
 /**
  * Routes for packing list
  */
-app.post("/api/packingLists", (req, res) => {
-  console.log(req.body, "Server index.js LINE 55");
+app.post('/api/packingLists', (req, res) => {
+  console.log(req.body, 'Server index.js LINE 55');
   PackingLists.create({
     listName: req.body.listName,
     packingListDescription: req.body.packingListDescription,
   })
     .then((data) => {
-      console.log("LINE 63", data.dataValues);
+      console.log('LINE 63', data.dataValues);
       res.sendStatus(201);
     })
     .catch((err) => {
-      console.error(err, "Something went wrong");
+      console.error(err, 'Something went wrong');
       res.sendStatus(500);
     });
 });
