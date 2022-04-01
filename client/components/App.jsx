c4d1393b10969745bc5282d7d59399f793e6fd30
Rene Mercadel <renemercadel@gmail.com>
2022-04-01 18:57:07 -0500

    Implemented Hashrouting to allow pages to refresh, trail profile image feed is ready to have css applied, implementing loading icon for trail profile image feed, made trail list

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 // Import Dependencies
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import axios from 'axios';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -16,6 +16,14 @@ import Login from './Login.jsx';
 const App = () => {
   const [trailList, setTrailList] = useState([]);
 
+  useEffect(() => {
+    if (localStorage.getItem('TrailList')) {
+      const trails = JSON.parse(localStorage.getItem('TrailList'));
+      setTrailList(trails);
+    }
+  }, []);
+
+  // were in trail list
   const handleGetTrails = (location) => {
     // e.preventDefault(); // don't need
     axios
@@ -24,9 +32,9 @@ const App = () => {
       })
       .then((response) => {
         // console.log("Line 24 = ", response.data.data); - returns array of objects of trail data
-        setTrailList(() => {
-          return [...response.data.data];
-        });
+        setTrailList(response.data.data);
+        // add data to local storage
+        localStorage.setItem('TrailList', JSON.stringify(response.data.data));
       })
       .catch((err) => {
         console.error('ERROR: ', err);
