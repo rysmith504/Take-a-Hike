fb435daa315e2caa3a059d1c51b0f9e394885a88
Bethany Jones <betpetjones@gmail.com>
2022-07-26 18:27:05 -0500

    create RDS connection

--- server/database/index.js
+++ server/database/index.js
@@ -1,24 +1,52 @@
 // Import Dependencies
-// const mysql = require("mysql2");
-const { Sequelize } = require("sequelize");
-
-// Initialized DB
-const db = new Sequelize("TakeAHike", "root", "", {
-  host: "localhost", // The `host` parameter is required for other databases
-  dialect: "mysql",
+const mysql = require("mysql2");
+require('dotenv').config();
+console.log(process.env.RDS_HOSTNAME);
+const db = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
 });
 
-// Use Sequelize Authenticate Method
-db.authenticate() // Runs a SELECT query and checks if the database responds correctly
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
-db.query("set foreign_key_checks = 0");
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
 
 // Export DB
 module.exports = {
   db,
 };
+
+// ORIGINAL ------------------------
+// // Import Dependencies
+// const mysql = require("mysql2");
+// const { Sequelize } = require("sequelize");
+
+// // Initialized DB
+// const db = new Sequelize("TakeAHike", "root", "", {
+//   host: "localhost", // The `host` parameter is required for other databases
+//   dialect: "mysql",
+// });
+
+// // Use Sequelize Authenticate Method
+// db.authenticate() // Runs a SELECT query and checks if the database responds correctly
+//   .then(() => {
+//     console.log("Connection has been established successfully.");
+//   })
+//   .catch((err) => {
+//     console.error("Unable to connect to the database:", err);
+//   });
+// db.query("set foreign_key_checks = 0");
+
+// // Export DB
+// module.exports = {
+//   db,
+// };
