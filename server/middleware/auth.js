7eea14e73871b12561f70ef0346867420e189819
Bethany Jones <betpetjones@gmail.com>
2022-07-26 13:15:28 -0500

    change port, update auths

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,12 +8,10 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { Users } = require('../database/models/users.js')
 
-
-
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5555/auth/google/callback",
+  callbackURL: "/auth/google/callback",
   passReqToCallback: true
 },
   async (req, accessToken, refreshToken, profile, done) => {
