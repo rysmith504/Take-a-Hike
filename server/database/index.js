094ec99d52a663506744b3f63ab4f1c8cd15fe48
Bethany Jones <betpetjones@gmail.com>
2022-07-26 18:57:20 -0500

    connect RDS successfully

--- server/database/index.js
+++ server/database/index.js
@@ -1,24 +1,30 @@
 // Import Dependencies
 const mysql = require("mysql2");
+const { Sequelize } = require("sequelize");
 require('dotenv').config();
-console.log(process.env.RDS_HOSTNAME);
-const db = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+
+// Initialized DB
+const db = new Sequelize("TakeAHike", process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
+  host: process.env.RDS_HOSTNAME, // The `host` parameter is required for other databases
+  port: process.env.RDS_PORT,
+  dialect: "mysql",
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
 
-  console.log('Connected to database.');
-});
+// Use Sequelize Authenticate Method
+db.authenticate() // Runs a SELECT query and checks if the database responds correctly
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+db.query("set foreign_key_checks = 0");
 
-connection.end();
+// Export DB
+module.exports = {
+  db,
+};
 
 // Export DB
 module.exports = {
