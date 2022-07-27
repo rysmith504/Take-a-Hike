eb4dbc8aec19f2b4f06181201d398aeb0d62584b
Vincent Tong <vincent.khang.tong@gmail.com>
2022-07-27 12:38:24 -0500

    weather component rendering important info

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -9,25 +9,37 @@ import { useEffect, useState } from "react";
 const Weather = () => {
   const [temp, setTemp] = useState(0);
   const [humidity, setHumidity] = useState(0);
+  const [weatherId, setWeatherId] = useState(0);
+  const [clouds, setClouds] = useState(0);
 
   const getWeather = () => {
     axios.get('/api/weather')
       .then((weatherObj) => {
         console.log(weatherObj);
+        setClouds(weatherObj.data.current.clouds);
         setTemp(weatherObj.data.current.temp)
         setHumidity(weatherObj.data.current.humidity);
+        setWeatherId(weatherObj.data.current.weather[0].id)
       })
       .catch((err) => console.error(err))
   }
   
   useEffect(() => {
     getWeather();
-  })
-
-  console.log(temp);
+  }, [])
 
   return (
     <div>
+      <div id='icon' class='weather' align='center'>
+      {(weatherId >= 300 && weatherId < 400) ? <img src='https://cdn3.iconfinder.com/data/icons/tiny-weather-1/512/flash-cloud-256.png'/> : ''}
+        {(weatherId >= 400 && weatherId < 500) ? <img src='https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather05-256.png'/> : ''}
+        {(weatherId >= 500 && weatherId < 600) ? <img src='https://cdn3.iconfinder.com/data/icons/tiny-weather-1/512/rain-512.png'/> : ''}
+        {(weatherId >= 600 && weatherId < 700) ? <img src='https://cdn3.iconfinder.com/data/icons/tiny-weather-1/512/snow-cloud-512.png'/> : ''}
+        {(weatherId > 700 && weatherId < 800) ? 'it is not ideal for hiking' : ''}
+        {(weatherId === 800) ? <img src='https://cdn3.iconfinder.com/data/icons/tiny-weather-1/512/sun-512.png'/> : ''}
+        {(weatherId > 800 && weatherId < 900) ? <img src='https://cdn3.iconfinder.com/data/icons/tiny-weather-1/512/cloudy-256.png'/> : ''}
+      </div>
+
       <div id='temp' class='weather'>
       The current temperature is {temp} degrees fahrenheit 
       </div>
@@ -36,7 +48,18 @@ const Weather = () => {
         Humidity is at {humidity} %
       </div>
 
-    
+      <div id='sky' class='weather'>
+        {(weatherId >= 300 && weatherId < 400) ? 'there is a thunderstorm' : ''}
+        {(weatherId >= 400 && weatherId < 500) ? 'it is drizzling' : ''}
+        {(weatherId >= 500 && weatherId < 600) ? 'it is raining' : ''}
+        {(weatherId >= 600 && weatherId < 700) ? 'it is snowy' : ''}
+        {(weatherId > 700 && weatherId < 800) ? 'it is not ideal for hiking' : ''}
+        {(weatherId === 800) ? 'the sky is clear' : ''}
+        {(weatherId > 800 && weatherId < 900) ? `it is ${clouds} % cloudy` : ''}
+      </div>
+
+
+
 
     </div>
   )
