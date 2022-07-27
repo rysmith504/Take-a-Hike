0a2d6e1a194081e22028831e7b645fdf52645593
Vincent Tong <vincent.khang.tong@gmail.com>
2022-07-27 10:44:28 -0500

    call to weather api moved to server side

--- client/components/Weather.jsx
+++ client/components/Weather.jsx
@@ -1,4 +1,3 @@
-import ENV from '../../map.env';
 import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from "react";
@@ -9,15 +8,14 @@ import { useEffect, useState } from "react";
 
 const Weather = () => {
   const [temp, setTemp] = useState(0);
-  let lat = 29.9430
-  let lon = -90.3517
-  const getWeather = () => {
-    axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&lang=en&exclude=minutely,hourly,alerts&appid=${ENV.WEATHER}`)
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
-  }
 
-getWeather()
+  const getWeather = () => {
+    axios.get('/api/weather')
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err))
+  }
+  
+  getWeather()
 
   return (
     <div>
