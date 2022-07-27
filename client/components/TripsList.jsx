b7071056b543db27e702cff9eedd27191ecba2ee
Bethany Jones <betpetjones@gmail.com>
2022-07-27 14:23:13 -0500

    install moment.js, conditional rendering in trips, axios for past trips

--- client/components/TripsList.jsx
+++ client/components/TripsList.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const TripsList = () => {
-<<<<<<< HEAD
   const [pastTrips, setPastTrips] = useState([]);
   const [tripsList, setTripsList] = useState([]);
   const [today, setToday] = useState([]);
@@ -21,13 +20,6 @@ const TripsList = () => {
       });
 
       axios.get('/api/trips')
-=======
-  const [tripsList, setTripsList] = useState([]);
-  useEffect(() => {
-
-// GET trips list
-    axios.get('/api/trips')
->>>>>>> c32e64e5b48d6fd499fe5b70506d2f43cdb385b7
       .then((response) => {
         console.log('got trips');
         setTripsList(response.data);
