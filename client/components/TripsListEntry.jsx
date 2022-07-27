bed07367a40ddfdfeb16d46de78ecc14b671315e
Bethany Jones <betpetjones@gmail.com>
2022-07-27 18:21:50 -0500

    Trips: add styles for weather icons, create icon component (#19)
    * add trips dummy data and seed DB
    
    * add trips dummy data, render trip details with axios
    
    * install moment.js, conditional rendering in trips, axios for past trips
    
    * Update TripsList.jsx
    
    * install moment.js, conditional rendering in trips, axios for past trips
    
    * add address to trip model, create axios get for coords
    
    * add styles for weather icons, create icon component

--- client/components/TripsListEntry.jsx
+++ client/components/TripsListEntry.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+import WeatherIcons from "./WeatherIcons.jsx";
+
 const moment = require('moment');
 const TripsListEntry = ({ trip }) => {
+    let current = moment();
+    let sevenDays = moment().add(7, 'days');
+    const [coordinates, setCoordinates] = useState([]);
+    const [weather, setWeather] = useState([]);
+    useEffect(() => {
+    // if a trip is within seven days from now
+    if(moment(trip.tripDate).isBetween(current, sevenDays)){
+      // get the coordinates from Google API
+      // use those coordinates to get the weather forecast
+      axios.get('/api/latLng', {
+        params: {
+        'city': trip.tripLocation,
+      }})
+      .then((response) => {
+        setCoordinates(response.data);
+        console.log(response.data);
+        axios.get('/api/weather', coordinates)
+        .then((results) => {
+          console.log(results.data);
+          setWeather(results.data)
+        })
+        .catch((err) => console.error(err));
+      })
+      .catch((err)=>{
+        console.error(err);
+      })
+   }
+  }, []);
   return (
     <div>
       <div className="trip-card">
@@ -15,6 +46,9 @@ const TripsListEntry = ({ trip }) => {
             <p>Location: {trip.tripLocation}</p>
           </div>
           <div className="info-group">
+            <p>Address: {trip.tripAddress}</p>
+          </div>
+          <div className="info-group">
             <p>Date: {moment(trip.tripDate).format('ll')}</p>
           </div>
           <div className="info-group">
@@ -24,8 +58,8 @@ const TripsListEntry = ({ trip }) => {
             : <p><em>Your trip was {moment(trip.tripDate, "YYYYMMDD").fromNow()}.
             </em></p> 
             }
-
           </div>
+          <WeatherIcons />
         </div>
       </div>
     </div>
