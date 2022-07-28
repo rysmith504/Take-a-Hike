71af6eac899d44da2ee5a584415a242e7d5656c6
Bethany Jones <betpetjones@gmail.com>
2022-07-28 14:53:13 -0500

    update nav bar with dropdowns, add new trip form

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -15,7 +15,7 @@ const Form = () => {
 <div class="field">
   <label class="label">Username</label>
   <div class="control has-icons-left has-icons-right">
-    <input class="input is-success" type="text" placeholder="Text input" value="bulma"/>
+    <input class="input" type="text" placeholder="Text input" value=""/>
     <span class="icon is-small is-left">
       <i class="fas fa-user"></i>
     </span>
@@ -23,13 +23,12 @@ const Form = () => {
       <i class="fas fa-check"></i>
     </span>
   </div>
-  <p class="help is-success">This username is available</p>
 </div>
 
 <div class="field">
   <label class="label">Email</label>
   <div class="control has-icons-left has-icons-right">
-    <input class="input is-danger" type="email" placeholder="Email input" value="hello@"/>
+    <input class="input" type="email" placeholder="Email input" value=""/>
     <span class="icon is-small is-left">
       <i class="fas fa-envelope"></i>
     </span>
@@ -37,7 +36,6 @@ const Form = () => {
       <i class="fas fa-exclamation-triangle"></i>
     </span>
   </div>
-  <p class="help is-danger">This email is invalid</p>
 </div>
 
 <div class="field">
