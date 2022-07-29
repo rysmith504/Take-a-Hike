4cffcb5e103dc760465524806a95219c9f0a7103
Vincent Tong <vincent.khang.tong@gmail.com>
2022-07-29 15:26:26 -0500

    gallery route added and page rendering

---
+++ client/components/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
+
+const Gallery = () => {
+
+  return (
+    <div>
+      GALLERY PAGE
+    </div>
+  )
+}
+
+export default Gallery
